f4602c3aa2405632066ce40054a57d7ceeee8be6
tibraj <tibraj94@gmail.com>
2020-12-31 10:20:43 -0500

    testing event listener for form submission

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ function Book (title, author, isbn) {
     this.author = author;
     this.isbn = isbn;
 }
+
+function UI() {
+
+}
+
+document.getElementById('book-form').addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('test');
+}) 
