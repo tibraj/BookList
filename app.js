4a95a8a2f8c44e9dc60b947e888b6dbbb286b971
tibraj <tibraj94@gmail.com>
2020-12-31 10:39:51 -0500

    validations for blank fields

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ function UI() {
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
     }
+
+    UI.prototype.showAlert = function(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        setTimeout(function(){
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
 }
 
 document.getElementById('book-form').addEventListener('submit', function(e){
@@ -32,6 +44,10 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     
     const book = new Book(title, author, isbn);
     const ui = new UI();
-    ui.addBookToList(book);
-    ui.clearFields();
+    if(title === '' || author === '' === isbn === '') {
+        ui.showAlert('Please Fill In All Fields')
+    } else {
+        ui.addBookToList(book);
+        ui.clearFields();
+    }
 }) 
